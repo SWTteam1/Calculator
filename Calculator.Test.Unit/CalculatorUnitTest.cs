cc28450e685af714316ac835a38055b3a7bb7c1f
armina <armina1506@hotmail.com>
2018-02-01 10:54:58 +0100

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        [Test]
+        public void Add_Add3and4_returns7()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.add(3,4), Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Minus_minus6and2_returns4()
+        {
+            var uut = new Calculator();
+            
+            Assert.That(uut.subtract(6,4), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Multiply_Multiply3and5_returns15()
+        {
+            var uut = new Calculator();
+            
+            Assert.That(uut.multiply(3,5), Is.EqualTo(15));
+     
+        }
+
+        [Test]
+        public void Power_Pow2exp4_returns16()
+        {
+            var uut = new Calculator();
+
+            Assert.That(uut.Power(2,4), Is.EqualTo(16));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+
+    }
+
+
+}
+        
